928535827ec24944e9a105ad625caa108c27de75
tonayam <tonayam99@gmail.com>
2022-05-22 11:41:35 +0100

    Update

---
+++ app.js
@@ -0,0 +1,15 @@
+// Vanilla Javascript
+var input = document.querySelector("#telephone");
+window.intlTelInput(input, {
+  // options here
+  initialCountry: "ng",
+});
+
+// jQuery
+$("#telephone").intlTelInput({
+  // options here
+  initialCountry: "ng",
+});
+
+// add a placeholder in the input with an example number for the selected country
+var isValid = iti.isValidNumber();
